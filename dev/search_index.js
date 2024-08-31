963f1c4a53510cc2a7cfc1993544b7dbf30611d2
Documenter.jl <documenter@juliadocs.github.io>
2024-08-31 19:31:47 +0000

    build based on 3c50177

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#CryptoGroups.jl","page":"CryptoGroups.jl","title":"CryptoGroups.jl","text":"","category":"section"},{"location":"","page":"CryptoGroups.jl","title":"CryptoGroups.jl","text":"Documentation for CryptoGroups.jl","category":"page"}]
+}
